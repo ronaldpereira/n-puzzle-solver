c95676b693394c859a036e59ba70baca1f6f40a4
Ronald <ronald.drp11@gmail.com>
2019-04-12 03:48:26 -0300

    converted indentation to spaces

--- src/libs/puzzle.py
+++ src/libs/puzzle.py
@@ -1,23 +1,25 @@
 import numpy as np
 
 class Puzzle:
-	def __init__(self, n, randomize=True):
-		self.n = n
-		self.createPuzzle(randomize)
-		print('puzzle')
-		print(self.puzzle)
+    def __init__(self, n, randomize=True):
+        self.n = n
+        self.createPuzzle(randomize)
+        print('puzzle')
+        print(self.puzzle)
 
-	def createPuzzle(self, randomize):
-		self.puzzle = np.append(np.arange(1, self.n**2), 0)
-		if randomize:
-			# np.random.shuffle(self.puzzle)
-			# self.puzzle = np.array([1, 2, 3, 4, 5, 6, 7, 0, 8]) # solucao 2 do n_puzzles
-			# self.puzzle = np.array([1, 5, 2, 0, 4, 3, 7, 8, 6]) # solucao 5 do n_puzzles
-			self.puzzle = np.array([5, 8, 2, 1, 0, 3, 4, 7, 6]) # solucao 10 do n_puzles
-			# self.puzzle = np.array([8, 6, 7, 2, 5, 4, 3, 0, 1]) # solucao 31 do n_puzzles
-		self.puzzle = self.puzzle.reshape(self.n, self.n)
+    def createPuzzle(self, randomize):
+        self.puzzle = np.append(np.arange(1, self.n**2), 0)
+        if randomize:
+            # np.random.shuffle(self.puzzle)
+            # self.puzzle = np.array([1, 2, 3, 4, 5, 6, 7, 0, 8]) # solucao 2 do npuzzle
+            # self.puzzle = np.array([1, 5, 2, 0, 4, 3, 7, 8, 6]) # solucao 5 do npuzzle
+            self.puzzle = np.array([5, 8, 2, 1, 0, 3, 4, 7, 6]) # solucao 10 do npuzzle
+            # self.puzzle = np.array([8, 0, 2, 5, 7, 3, 1, 4, 6]) # solucao 15 do npuzzle
+            # self.puzzle = np.array([8, 7, 0, 5, 4, 2, 1, 6, 3]) # solucao 20 do npuzzle
+            # self.puzzle = np.array([8, 6, 7, 2, 5, 4, 3, 0, 1]) # solucao 31 do npuzzle
+        self.puzzle = self.puzzle.reshape(self.n, self.n)
 
 
 class AnswerPuzzle(Puzzle):
-	def __init__(self, n):
-		super(AnswerPuzzle, self).__init__(n, False)
+    def __init__(self, n):
+        super(AnswerPuzzle, self).__init__(n, False)
--- src/libs/stateTree.py
+++ src/libs/stateTree.py
@@ -3,57 +3,57 @@ from copy import deepcopy
 import numpy as np
 
 class StateTree:
-	def __init__(self, puzzle, n):
-		self.n = n
-		self.puzzle = puzzle
-		self.up = None
-		self.down = None
-		self.left = None
-		self.right = None
+    def __init__(self, puzzle, n):
+        self.n = n
+        self.puzzle = puzzle
+        self.up = None
+        self.down = None
+        self.left = None
+        self.right = None
 
-	def expand(self):
-		spaceX, spaceY = self.findEmptySpace()
+    def expand(self):
+        spaceX, spaceY = self.findEmptySpace()
 
-		if spaceX > 0:
-			upPuzzle = self.moveSpaceUp(spaceX, spaceY)
-			self.up = StateTree(upPuzzle, self.n)
+        if spaceX > 0:
+            upPuzzle = self.moveSpaceUp(spaceX, spaceY)
+            self.up = StateTree(upPuzzle, self.n)
 
-		if spaceX < self.n-1:
-			downPuzzle = self.moveSpaceDown(spaceX, spaceY)
-			self.down = StateTree(downPuzzle, self.n)
+        if spaceX < self.n-1:
+            downPuzzle = self.moveSpaceDown(spaceX, spaceY)
+            self.down = StateTree(downPuzzle, self.n)
 
-		if spaceY > 0:
-			leftPuzzle = self.moveSpaceLeft(spaceX, spaceY)
-			self.left = StateTree(leftPuzzle, self.n)
+        if spaceY > 0:
+            leftPuzzle = self.moveSpaceLeft(spaceX, spaceY)
+            self.left = StateTree(leftPuzzle, self.n)
 
-		if spaceY < self.n-1:
-			rightPuzzle = self.moveSpaceRight(spaceX, spaceY)
-			self.right = StateTree(rightPuzzle, self.n)		
+        if spaceY < self.n-1:
+            rightPuzzle = self.moveSpaceRight(spaceX, spaceY)
+            self.right = StateTree(rightPuzzle, self.n)		
 
-	def moveSpaceUp(self, spaceX, spaceY):
-		upPuzzle = deepcopy(self.puzzle)
-		upPuzzle[spaceX, spaceY], upPuzzle[spaceX-1, spaceY] = upPuzzle[spaceX-1, spaceY], upPuzzle[spaceX, spaceY]
+    def moveSpaceUp(self, spaceX, spaceY):
+        upPuzzle = deepcopy(self.puzzle)
+        upPuzzle[spaceX, spaceY], upPuzzle[spaceX-1, spaceY] = upPuzzle[spaceX-1, spaceY], upPuzzle[spaceX, spaceY]
 
-		return upPuzzle
+        return upPuzzle
 
-	def moveSpaceDown(self, spaceX, spaceY):
-		downPuzzle = deepcopy(self.puzzle)
-		downPuzzle[spaceX, spaceY], downPuzzle[spaceX+1, spaceY] = downPuzzle[spaceX+1, spaceY], downPuzzle[spaceX, spaceY]
+    def moveSpaceDown(self, spaceX, spaceY):
+        downPuzzle = deepcopy(self.puzzle)
+        downPuzzle[spaceX, spaceY], downPuzzle[spaceX+1, spaceY] = downPuzzle[spaceX+1, spaceY], downPuzzle[spaceX, spaceY]
 
-		return downPuzzle
+        return downPuzzle
 
-	def moveSpaceLeft(self, spaceX, spaceY):
-		leftPuzzle = deepcopy(self.puzzle)
-		leftPuzzle[spaceX, spaceY], leftPuzzle[spaceX, spaceY-1] = leftPuzzle[spaceX, spaceY-1], leftPuzzle[spaceX, spaceY]
+    def moveSpaceLeft(self, spaceX, spaceY):
+        leftPuzzle = deepcopy(self.puzzle)
+        leftPuzzle[spaceX, spaceY], leftPuzzle[spaceX, spaceY-1] = leftPuzzle[spaceX, spaceY-1], leftPuzzle[spaceX, spaceY]
 
-		return leftPuzzle
+        return leftPuzzle
 
-	def moveSpaceRight(self, spaceX, spaceY):
-		rightPuzzle = deepcopy(self.puzzle)
-		rightPuzzle[spaceX, spaceY], rightPuzzle[spaceX, spaceY+1] = rightPuzzle[spaceX, spaceY+1], rightPuzzle[spaceX, spaceY]
+    def moveSpaceRight(self, spaceX, spaceY):
+        rightPuzzle = deepcopy(self.puzzle)
+        rightPuzzle[spaceX, spaceY], rightPuzzle[spaceX, spaceY+1] = rightPuzzle[spaceX, spaceY+1], rightPuzzle[spaceX, spaceY]
 
-		return rightPuzzle
+        return rightPuzzle
 
-	def findEmptySpace(self):
-		coord = np.where(self.puzzle == 0)
-		return coord[0][0], coord[1][0]
+    def findEmptySpace(self):
+        coord = np.where(self.puzzle == 0)
+        return coord[0][0], coord[1][0]
